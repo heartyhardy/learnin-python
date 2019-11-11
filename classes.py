1f1661c11091c72f880dc872e982de44653a8b25
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-11-11 05:58:45 +0530

    Added Classes

---
+++ classes.py
@@ -0,0 +1,36 @@
+''' Inheritance '''
+class Shape:
+    def draw(self):
+        print("A shape")
+
+class Circle(Shape):
+    pass
+
+circle = Circle()
+circle.draw()
+
+''' Classes and Constructor '''
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def talk(self):
+#         print(f'Hi my name is {self.name} and I\'m {self.age} years old.')
+
+
+# charith = Person(name="Charith", age=38)
+# charith.talk()
+
+
+# class Canvas:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+
+#     def draw_circle(self):
+#         print(self.x, self.y)
+
+
+# canvas = Canvas(10, 20)
+# canvas.draw_circle()
